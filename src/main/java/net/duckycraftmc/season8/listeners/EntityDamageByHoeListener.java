573da4b7ba2003f6c33d734d0fff7444e21ecd29
Blasty <rmzha2234@gmail.com>
2023-02-11 21:07:07 +1100

    reverse kb with hoes (hopefully)

--- src/main/java/net/duckycraftmc/season8/listeners/EntityDamageByHoeListener.java
+++ src/main/java/net/duckycraftmc/season8/listeners/EntityDamageByHoeListener.java
@@ -1,6 +1,7 @@
 package net.duckycraftmc.season8.listeners;
 
 import net.duckycraftmc.season8.Duckycraft;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,18 +22,25 @@ public class EntityDamageByHoeListener implements Listener {
     public void entityDamageByEntityEvent(EntityDamageByEntityEvent e) {
         if (!(e.getDamager() instanceof Player)) return;
         Player player = (Player) e.getDamager();
+        Entity damagedEntity = e.getEntity();
         ItemStack weapon = player.getInventory().getItemInMainHand();
         if (weapon.equals(WOODEN_HOE)) {
+            damagedEntity.setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(1.5));
         }
         if (weapon.equals(STONE_HOE)) {
+            damagedEntity.setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(2));
         }
         if (weapon.equals(IRON_HOE)) {
+            damagedEntity.setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(2.5));
         }
         if (weapon.equals(GOLDEN_HOE)) {
+            damagedEntity.setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(3));
         }
         if (weapon.equals(DIAMOND_HOE)) {
+            damagedEntity.setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(3.5));
         }
         if (weapon.equals(NETHERITE_HOE)) {
+            damagedEntity.setVelocity(player.getLocation().getDirection().setY(0).normalize().multiply(5));
         }
     }
 
